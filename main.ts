53bbc09b5dd75ebd1c54b013d39fae32982f5f0a
ana sofía <112406721+anasofiamenendez@users.noreply.github.com>
2022-10-14 13:40:26 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
+input.onPinPressed(TouchPin.P0, function () {
+    control.reset()
+})
 let paco = game.createSprite(0, 2)
+let daniela = game.createSprite(randint(1, 4), randint(0, 4))
+let patrick = game.createSprite(randint(1, 4), randint(0, 4))
+let josé = game.createSprite(randint(1, 4), randint(0, 4))
+paco.set(LedSpriteProperty.Brightness, 120)
 basic.forever(function () {
     while (input.buttonIsPressed(Button.AB)) {
         paco.change(LedSpriteProperty.Y, 1)
@@ -13,7 +20,33 @@ basic.forever(function () {
         basic.pause(100)
     }
     while (input.buttonIsPressed(Button.B)) {
-        paco.change(LedSpriteProperty.X, -1)
+        paco.change(LedSpriteProperty.X, 1)
         basic.pause(100)
+    }
+    if (paco.isTouching(daniela)) {
+        music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.OnceInBackground)
+        daniela.delete()
+        for (let index = 0; index < 3; index++) {
+            basic.showIcon(IconNames.Yes)
+            basic.clearScreen()
+        }
+    }
+    if (paco.isTouching(josé)) {
+        music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+        josé.delete()
+        for (let index = 0; index < 3; index++) {
+            basic.showIcon(IconNames.Yes)
+            basic.clearScreen()
+        }
+        game.addScore(1)
+    }
+    if (paco.isTouching(patrick)) {
+        music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+        patrick.delete()
+        for (let index = 0; index < 3; index++) {
+            basic.showIcon(IconNames.Yes)
+            basic.clearScreen()
+        }
+        game.addScore(1)
     }
 })
